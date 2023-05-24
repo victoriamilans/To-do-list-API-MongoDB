ceb83813c3dec4321c584261ce4e92511053c258
Victoria <victoria-milan@outlook.com>
2023-05-24 08:47:43 -0300

    feat/ create initial routes for users

---
+++ src/routes/users.routes.ts
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import { usersController } from "../controllers/users.controllers";
+
+const userRoutes = Router();
+
+userRoutes.post("", (req, res) => {
+  usersController.userCreate(req, res);
+});
+
+userRoutes.get("", (req, res) => {
+  usersController.listAllUsers(req, res);
+});
+
+userRoutes.get("/:id", (req, res) => {
+  usersController.listOneUser(req, res);
+});
+
+userRoutes.patch("/:id", (req, res) => {
+  usersController.userUpdate(req, res);
+});
+
+userRoutes.delete("/:id", (req, res) => {
+  usersController.deleteUser(req, res);
+});
+
+export { userRoutes };
